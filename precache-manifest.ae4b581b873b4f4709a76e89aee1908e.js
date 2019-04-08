b678316e3a2a51027cc357e6b61372657de785af
ivan Jankovic <jankoemail@gmail.com>
2019-04-08 12:54:47 -0400

    Updates

---
+++ precache-manifest.ae4b581b873b4f4709a76e89aee1908e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ivanjankovic.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5061c57d385430248ea",
+    "url": "/ivanjankovic.github.io/static/js/runtime~main.7f334519.js"
+  },
+  {
+    "revision": "b5c018ed9cb7fe3f3c4b",
+    "url": "/ivanjankovic.github.io/static/js/main.28966092.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ivanjankovic.github.io/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b5c018ed9cb7fe3f3c4b",
+    "url": "/ivanjankovic.github.io/static/css/main.e66754e2.chunk.css"
+  },
+  {
+    "revision": "e0c27a9601552ff9941c1fb75c6139cc",
+    "url": "/ivanjankovic.github.io/index.html"
+  }
+];
